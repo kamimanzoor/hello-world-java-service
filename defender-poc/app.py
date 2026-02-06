be473ea70b6d141112627e262234b1ea57207292
kamimanzoor <kami.manzoor@gmail.com>
2026-02-06 10:14:37 +0100

    feat: add initial app.py with main function [skip ci]

---
+++ defender-poc/app.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+def main():
+    print("Hello from an unsigned executable 👋")
+    print("If you see this, SmartScreen allowed execution.")
+    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
